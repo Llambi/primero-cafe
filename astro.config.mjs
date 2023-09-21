3335b2e862cc730a051cd150d5db21f9f11722a8
Hugo Perez <hugopf.mail@gmail.com>
2023-09-21 20:51:44 +0200

    chore: Improve compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import compress from 'astro-compress';
+import react from '@astrojs/react';
 
-import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), react()],
-	site: 'https://llambi.github.io',
-	base: '/primero-cafe'
-})
+  integrations: [tailwind(), react(), compress()],
+  site: 'https://llambi.github.io',
+  base: '/primero-cafe'
+});
